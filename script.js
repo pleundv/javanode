5329f8a3c34b0078bb71c8fa7dad7542a5eeb0a0
pleundv <84378584+pleundv@users.noreply.github.com>
2021-06-02 11:40:37 +0200

    script

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hallo Winc Academy studenten")
+
+/*
+let multiplication = 1230941 * 1823794;
+console.log (multiplication)
+
+let gedeelte = 637263 / 54;
+console.log (gedeelte)
+
+*/
+
